096a63ba7d4eb299f04715106c83f489fd32d91e
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-12 17:04:31 +0900

    added event listeners

--- src/App.js
+++ src/App.js
@@ -3,12 +3,50 @@ import React from "react";
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      firstName: "",
+      lastName: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(event) {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  // THE NAME AND VALUE ATTRIBUTES ABOVE WERE PULLED OUT OF THE TARGET AND ASSIGNED AS VARIABLES. Same as below:
+  // handleChange(event) {
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   })
+  // }
+
   render() {
     return (
       <div>
-        <h1>Testing</h1>
+        <h1>Form Practice</h1>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            placeholder="First Name"
+            onChange={this.handleChange}
+          />
+          <br/>
+          <br/>
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            placeholder="Last Name"
+            onChange={this.handleChange}
+          />
+          <h1>Full name: {this.state.firstName} {this.state.lastName}</h1>
+        </form>
       </div>
     )
   }
