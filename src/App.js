b0d5586d32edb073e71f79bf7dd8bb920abf03d0
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-15 02:26:04 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -9,21 +9,25 @@ class App extends React.Component {
       age: "",
       gender: "",
       destination: "",
-      dietVeg: "",
-      dietLac: "",
-      dietAll: ""
+      isVegetarian: "",
+      isLactoseIntolerant: "",
+      isAllergic: "",
+      additionalDetails: ""
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event) {
     const {name, value, type, checked} = event.target
-    type === "checkbox" ? this.setState({[name]: checked}) : this.setState({[name]: value})
+    type === "checkbox" ? this.setState({[name]: checked }) : this.setState({[name]: value})
   }
 
   handleSubmit() {
+    const maleOrFemale = this.state.gender === "Male" ? "Mr." : "Ms.";
     alert (
-      "First name": {this.state.firstName});
+      `Thank you for submitting the form, ${maleOrFemale} ${this.state.lastName}.`
+    );
   }
 
   // THE NAME AND VALUE ATTRIBUTES ABOVE WERE PULLED OUT OF THE TARGET AND ASSIGNED AS VARIABLES. Same as below:
@@ -36,8 +40,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="container-main">
-        <form onSubmit={this.handleSubmit} className="questions shadow border">
-          <h1>Passenger</h1>
+        <form className="questions shadow border">
+          <h1>Passenger Info</h1>
           <input
             type="text"
             name="firstName"
@@ -45,82 +49,99 @@ class App extends React.Component {
             placeholder="First Name"
             onChange={this.handleChange}
           />
+          <br />          
           <input
             type="text"
             name="lastName"
             value={this.state.lastName}
-            placeHolder="Last Name"
+            placeholder="Last Name"
             onChange={this.handleChange}
           />
+          <br />
           <input
             type="number"
             name="age"
             value={this.state.age}
+            placeholder="Age"
             onChange={this.handleChange}
           />
+          <br />
           <label>
             <input
               type="radio"
               name="gender"
-              value="male"
-              checked={this.state.gender === "male"}
+              value="Male"
+              checked={this.state.gender === "Male"}
               onChange={this.handleChange}
             /> Male
+            <br />
             <input
               type="radio"
               name="gender"
-              value="female"
-              checked={this.state.gender === "female"}
+              value="Female"
+              checked={this.state.gender === "Female"}
               onChange={this.handleChange}
             /> Female
           </label>
-          <label>Destination:</label>
+          <br />
           <select
             name="destination"
             value={this.state.destination}
             onChange={this.handleChange}
           >
-            <option value="australia">Australia</option>
-            <option value="thailand">Thailand</option>
-            <option value="singapore">Singapore</option>
+            <option value="">--Select a destination--</option>
+            <option value="Australia">Australia</option>
+            <option value="Thailand">Thailand</option>
+            <option value="Singapore">Singapore</option>
           </select>
+          <br />
           <label>
             <input
               type="checkbox"
-              name="dietVeg"
-              checked={this.state.dietVeg}
+              name="isVegetarian"
+              checked={this.state.isVegetarian}
               onChange={this.handleChange}
-            />Vegetarian
+            /> Vegetarian
           </label>
           <label>
             <input
               type="checkbox"
-              name="dietLac"
-              checked={this.state.dietLac}
+              name="isLactoseIntolerant"
+              checked={this.state.isLactoseIntolerant}
               onChange={this.handleChange}
-            />Lactose Intolerant
+            /> Lactose Intolerant
           </label>
           <label>
             <input
               type="checkbox"
-              name="dietAll"
-              checked={this.state.dietAll}
+              name="isAllergic"
+              checked={this.state.isAllergic}
               onChange={this.handleChange}
-            />Food Allergies
+            /> Food Allergies
           </label>
-          <button className="btn btn-primary">Submit</button>
+          <br />
+          <textarea
+            name="additionalDetails"
+            value={this.state.additionalDetails}
+            placeholder="Additional details..."
+            onChange={this.handleChange}
+            rows="3"
+            cols="30"
+          />
         </form>
-        <div className="results shadow border">
-          <h1>Your Details</h1>
-          <ul>
-            <li>Full name: {this.state.firstName} {this.state.lastName}</li>
-            <li>Age: {this.state.age}</li>
-            <li>Gender: {this.state.gender}</li>
-            <li>Destination: {this.state.destination}</li>
-            <li>Additional details: {this.state.details}</li>
-            <li>Dietery Restrictions: {this.state.dietVeg}</li>
-          </ul>
-        </div>
+        <form onSubmit={this.handleSubmit} className="results shadow border">
+          <h1>Confirmation</h1>
+            <p>Full name: <b>{this.state.firstName} {this.state.lastName}</b></p>
+            <p>Age: <b>{this.state.age}</b></p>
+            <p>Gender: <b>{this.state.gender}</b></p>
+            <p>Destination: <b>{this.state.destination}</b></p>
+            <p>Vegetarian: <b>{this.state.isVegetarian ? "Yes" : "No"}</b></p>
+            <p>Lactose Intolerant: <b>{this.state.isLactoseIntolerant ? "Yes" : "No"}</b></p>
+            <p>Allergic: <b>{this.state.isAllergic ? "Yes" : "No"}</b></p>
+            <p>Additional details: <b>{this.state.additionalDetails}</b></p>
+          <hr />
+          <button className="btn btn-primary" type="submit">Submit</button>
+        </form>
       </div>
     )
   }
@@ -128,65 +149,3 @@ class App extends React.Component {
 
 
 export default App;
-
-{/* <br/>
-<br/>
-<input
-  type="text"
-  name="lastName"
-  value={this.state.lastName}
-  placeholder="Last Name"
-  onChange={this.handleChange}
-/>
-<br/>
-<br />
-<textarea
-  name="description"
-  value={this.state.description}
-  placeholder="Describe yourself"
-  onChange={this.handleChange}
-/>
-<br />
-<br />
-<label>
-  <input
-    type="checkbox"
-    name="isAustralian"
-    checked={this.state.isAustralian}
-    onChange={this.handleChange}
-  /> Are you Australian?
-</label>
-<br />
-<br />
-<label>
-  <input
-    type="radio"
-    name="gender"
-    value="male"
-    checked={this.state.gender === "male"}
-    onChange={this.handleChange}
-  /> Male
-</label>
-<label>
-  <input
-    type="radio"
-    name="gender"
-    value="female"
-    checked={this.state.gender === "female"}
-    onChange={this.handleChange}
-  /> Female
-</label>
-<br />
-<br />
-<label>Favorite color:</label>
-<select
-  name="favColor"
-  value={this.state.favColor}
-  onChange={this.handleChange}
->
-  <option value="green">Green</option>
-  <option value="blue">Blue</option>
-  <option value="red">Red</option>
-</select>
-<br />
-<button className="btn btn-primary">Submit</button> */}
